8e3939ff07e7cfa3c96e7cf9d2af080dc2899edc
omar elroby <58413280+omarelroby@users.noreply.github.com>
2024-12-26 05:33:01 +0200

    header

--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -51,5 +51,18 @@
 	<link rel="stylesheet" href="{{ asset('public/assets/css/style.css')}}">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
-    
+    <!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- Moment.js CDN -->
+<script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js"></script>
+
+<!-- Moment Hijri CDN -->
+<script src="https://cdn.jsdelivr.net/npm/moment-hijri@2.17.0/moment-hijri.min.js"></script>
+
+<!-- jQuery datetimepicker -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/datetimepicker@2.5.4/build/css/bootstrap-datetimepicker.min.css">
+<script src="https://cdn.jsdelivr.net/npm/datetimepicker@2.5.4/build/js/bootstrap-datetimepicker.min.js"></script>
+
+
 </head>
